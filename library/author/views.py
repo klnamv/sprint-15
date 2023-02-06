84118025b80399f58beed20d2fd8b3198b8dffee
DKl1 <klymbag@gmail.com>
2023-02-06 22:36:01 +0200

    add simple views to author

--- library/author/views.py
+++ library/author/views.py
@@ -1,1 +1,34 @@
-# Create your views here.
+from django.shortcuts import render
+from django.db.models import Q
+from django.views.decorators.csrf import csrf_exempt
+
+from . models import Author
+from book.models import Book
+
+
+
+def authors_info(request):
+    authors = list(Author.get_all())
+    context = {'authors': authors}
+    return render(request, 'authors_info.html', context)
+
+
+@csrf_exempt
+def create_author(request):
+    name = request.POST.get('author_name')
+    surname = request.POST.get('surname')
+    patronymic = request.POST.get('patronymic')
+    Author.create(name, surname, patronymic)
+
+    return render(request, 'create_author.html')
+
+
+@csrf_exempt
+def remove_author(request):
+    author_id = request.POST.get('author_id')
+    author = Author.get_by_id(author_id)
+
+    if len(Book.objects.filter(authors=author)) <= 0:
+        Author.delete_by_id(author_id)
+
+    return render(request, 'remove_author.html')
