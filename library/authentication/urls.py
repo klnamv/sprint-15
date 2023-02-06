f70a00cdaf9823e489251bffa6a035d18b079701
DKl1 <klymbag@gmail.com>
2023-02-06 12:47:08 +0200

    change url for user, from 'users' to 'user'

--- library/authentication/urls.py
+++ library/authentication/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('users/', views.users_info),
+    path('user/', views.users_info),
     path('user/<int:id>/', views.user_info)
 ]
